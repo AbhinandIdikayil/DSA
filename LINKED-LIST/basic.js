43f8f1821eecd723d203df4bde306bed17337ed1
Abhinand <abhin4nd6@gmail.com>
2024-05-13 10:40:09 +0530

    pop,unshift and remove by value

--- LINKED-LIST/basic.js
+++ LINKED-LIST/basic.js
@@ -1,6 +1,6 @@
 
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value
         this.next = null;
     }
@@ -11,16 +11,16 @@ class List {
         this.head = null;
         this.size = 0
     }
-    isEmpty(){
+    isEmpty() {
         return this.size === 0
     }
     append(value) {
         let node = new Node(value);
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
             this.head = node
-        }else{
+        } else {
             let current = this.head
-            while(current.next){
+            while (current.next) {
                 current = current.next
             }
             current.next = node
@@ -28,10 +28,50 @@ class List {
         this.size++;
     }
 
-    display(){
+    removeValue(value) {
+        if (this.isEmpty()) {
+            return null
+        }
+        if (this.head.value == value) {
+            this.unShift()
+        } else {
+            let current = this.head.next
+            let previous = null
+            while (current.value != value && current.next) {
+                previous = current
+                current = current.next
+            }
+            previous.next = current.next
+        }
+    }
+
+    unShift() {
+        if (this.isEmpty()) {
+            return null
+        } else {
+            this.head = this.head.next
+        }
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return null
+        } else {
+            let current = this.head
+            let previous = null
+            while (current.next) {
+                previous = current
+                current = current.next
+            }
+            previous.next = null
+            this.size--
+        }
+    }
+
+    display() {
         let values = ''
         let current = this.head
-        while(current) {
+        while (current) {
             values += current.value + ' '
             current = current.next
         }
@@ -43,4 +83,8 @@ let list = new List()
 list.append(1)
 list.append(2)
 list.append(3)
+list.append(4)
+list.append(5)
+list.append(6)
+list.removeValue(4)
 list.display()
