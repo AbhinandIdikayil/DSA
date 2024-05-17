40ed1f17b4294f7c462eb8a9459b343f32610c5a
Abhinand <abhin4nd6@gmail.com>
2024-05-17 16:57:48 +0530

    bubbl sort

---
+++ SORTING/bubbleSort.js
@@ -0,0 +1,17 @@
+let arr = [6,3,4,8,1]
+function bubble(arr) {
+    let swapped;
+    do {
+        swapped = false
+        for (let i = 0; i < arr.length-1; i++) {
+            if(arr[i] > arr[i+1]) {
+                let temp = arr[i];
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }        
+        }
+    } while (swapped);
+}
+bubble(arr)
+console.log(arr)
